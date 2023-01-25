77133e0a70f83aac4c694acf3f9a8819435f3a86
CebuCoder <55612034+Cebucoder@users.noreply.github.com>
2023-01-25 05:41:44 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+const tl = gsap.timeline({defaults: {duration: 1}})
+
+tl.fromTo('.caption', {x: -100, opacity: 0}, {x: 0, opacity: 1})
+tl.fromTo('.carousel-inner', {x: 100, opacity: 0}, {x: 0, opacity: 1},'<')
+tl.fromTo('.carousel-inner', {y: -20}, {y: 0, yoyo: true, repeat: -1},'<')
+
+AOS.init({
+  duration: 1200,
+})
